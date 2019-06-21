a547798eff885dcbfdf424b4a480f7a5019debae
Abbath1349 <666ya1349@mail.ru>
2019-06-22 02:42:21 +0300

    show data

---
+++ app/src/main/java/com/dmitry_kuzmin/ratesapp/rates/presentation/RateListAdapter.kt
@@ -0,0 +1,58 @@
+package com.dmitry_kuzmin.ratesapp.rates.presentation
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dmitry_kuzmin.ratesapp.R
+import com.dmitry_kuzmin.ratesapp.menu.domain.model.Stock
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_rate.*
+
+private typealias onStockClick = (stock: Stock) -> Unit
+
+class RateListAdapter(private val onStockClick: onStockClick) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val items = arrayListOf<Stock>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_rate, parent, false)
+        return StockViewHolder(view, onStockClick)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is StockViewHolder) {
+            holder.bind(items[position])
+        } else {
+            throw RuntimeException("Wrong holder")
+        }
+    }
+
+    fun updateItems(items: List<Stock>) {
+        this.items.clear()
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    private class StockViewHolder(view: View, private val onStockClick: onStockClick) :
+        RecyclerView.ViewHolder(view),
+        LayoutContainer {
+
+        override val containerView: View?
+            get() = itemView
+
+        @SuppressLint("SetTextI18n")
+        fun bind(stock: Stock) {
+            itemView.setOnClickListener {
+                onStockClick(stock)
+            }
+            symbolTextView.text = stock.symbol
+            nameTextView.text = stock.companyName
+            priceTextView.text = "${itemView.context.getString(R.string.price)}: ${stock.price}"
+            rangeTextView.text = "${itemView.context.getString(R.string.range)}: ${stock.rangeLow} - ${stock.rangeHigh}"
+        }
+    }
+}
--- app/src/main/java/com/dmitry_kuzmin/ratesapp/rates/presentation/ui/fragment/RateListFragment.kt
+++ app/src/main/java/com/dmitry_kuzmin/ratesapp/rates/presentation/ui/fragment/RateListFragment.kt
@@ -4,22 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dmitry_kuzmin.ratesapp.R
 import com.dmitry_kuzmin.ratesapp.menu.domain.model.Stock
+import com.dmitry_kuzmin.ratesapp.rates.presentation.RateListAdapter
 import com.dmitry_kuzmin.ratesapp.rates.presentation.RateListPresenter
 import com.dmitry_kuzmin.ratesapp.rates.presentation.contracts.IRatesList
 import com.hannesdorfmann.mosby3.mvi.MviFragment
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.fragment_rates_list.*
 
 class RateListFragment : MviFragment<IRatesList.View, RateListPresenter>(), IRatesList.View {
+
+    private lateinit var adapter: RateListAdapter
 
     override fun createPresenter(): RateListPresenter {
         return RateListPresenter()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = RateListAdapter { }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.fragment_rates_list, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ratesRecyclerView.layoutManager = LinearLayoutManager(context)
+        ratesRecyclerView.adapter = adapter
+    }
 
     override fun loadIntents(): Observable<String> {
         return Observable.just("Hello")
@@ -38,7 +53,7 @@ class RateListFragment : MviFragment<IRatesList.View, RateListPresenter>(), IRat
     }
 
     private fun renderContent(stocks: List<Stock>) {
-        val size = stocks.size
+        adapter.updateItems(stocks)
     }
 
     private fun renderError() {
